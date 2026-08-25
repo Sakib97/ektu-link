bfe6a86336c9804e0b370f0d8fda6a825b130c53
Sakib97 <ashekseum86@gmail.com>
2026-08-26 05:49:34 +0600

    Enhance NavigationBar with new toggle icon and styling; improve CSS for better hover effects and focus states.

--- src/components/layout/NavigationBar.jsx
+++ src/components/layout/NavigationBar.jsx
@@ -4,6 +4,9 @@ import Navbar from "react-bootstrap/Navbar";
 import Offcanvas from "react-bootstrap/Offcanvas";
 import styles from "./NavigationBar.module.css";
 import logo from "/link.png";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import { CgMenuLeftAlt } from "react-icons/cg";
+import { RiMenu5Fill } from "react-icons/ri";
 
 const NavigationBar = () => {
   return (
@@ -13,7 +16,14 @@ const NavigationBar = () => {
           <img src={logo} alt="ektu link" className={styles.logo} />
           <span>ektu link</span>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbar-offcanvas" />
+
+        <Navbar.Toggle
+          aria-controls="navbar-offcanvas"
+          className={styles.toggle}
+        >
+          <CgMenuLeftAlt size={28} />
+        </Navbar.Toggle>
+
         <Navbar.Offcanvas
           id="navbar-offcanvas"
           aria-labelledby="navbar-offcanvas-label"
@@ -25,13 +35,23 @@ const NavigationBar = () => {
           </Offcanvas.Header>
           <Offcanvas.Body>
             <Nav className="mx-auto">
-              <Nav.Link href="#features" className={styles.navLink}>Features</Nav.Link>
-              <Nav.Link href="#pricing" className={styles.navLink}>Pricing</Nav.Link>
-              <Nav.Link href="#api-docs" className={styles.navLink}>API Docs</Nav.Link>
+              <Nav.Link href="#features" className={styles.navLink}>
+                Features
+              </Nav.Link>
+              <Nav.Link href="#pricing" className={styles.navLink}>
+                Pricing
+              </Nav.Link>
+              <Nav.Link href="#api-docs" className={styles.navLink}>
+                API Docs
+              </Nav.Link>
             </Nav>
             <Nav className={styles.authNav}>
-              <Nav.Link href="#signin" className={styles.signIn}>Sign In</Nav.Link>
-              <Nav.Link href="#get-started" className={styles.getStarted}>Get Started</Nav.Link>
+              <Nav.Link href="#signin" className={styles.signIn}>
+                Sign In
+              </Nav.Link>
+              <Nav.Link href="#get-started" className={styles.getStarted}>
+                Get Started
+              </Nav.Link>
             </Nav>
           </Offcanvas.Body>
         </Navbar.Offcanvas>
